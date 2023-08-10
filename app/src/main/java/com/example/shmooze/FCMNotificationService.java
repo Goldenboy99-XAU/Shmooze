03579f79d785010dbcb37e771fe7844f600f8f6f
Goldenboy99-XAU <110536745+Goldenboy99-XAU@users.noreply.github.com>
2023-08-11 01:15:24 +0200

    Complete commit

---
+++ app/src/main/java/com/example/shmooze/FCMNotificationService.java
@@ -0,0 +1,6 @@
+package com.example.shmooze;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class FCMNotificationService extends FirebaseMessagingService {
+}
---
+++ app/src/main/java/com/example/shmooze/LoginUsernameActivity.java
@@ -0,0 +1,100 @@
+package com.example.shmooze;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.example.shmooze.model.UserModel;
+import com.example.shmooze.utils.FirebaseUtil;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentSnapshot;
+
+public class LoginUsernameActivity extends AppCompatActivity {
+
+    EditText usernameInput;
+    Button loginEnterBtn;
+    ProgressBar progressBar;
+    String phoneNumber;
+    UserModel userModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_username);
+
+        usernameInput = findViewById(R.id.login_username);
+        loginEnterBtn = findViewById(R.id.Login_enter_btn);
+        progressBar = findViewById(R.id.login_progress_bar);
+
+        phoneNumber = getIntent().getExtras().getString("phone");
+        getUsername();
+
+        loginEnterBtn.setOnClickListener((v -> {
+            setUsername();
+        }));
+
+    }
+
+    void setUsername(){
+
+        String username = usernameInput.getText().toString();
+        if (username.isEmpty() || username.length() < 3) {
+            usernameInput.setError("Username length should be at least 3 characters");
+            return;
+        }
+        setInProgress(true);
+        if(userModel!=null){
+            userModel.setUsername(username);
+        }else{
+            userModel = new UserModel(phoneNumber,username, Timestamp.now(),FirebaseUtil.currentUserId());
+        }
+
+        FirebaseUtil.currentUserDetails().set(userModel).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                setInProgress(false);
+                if(task.isSuccessful()){
+                    Intent intent = new Intent(LoginUsernameActivity.this,MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+
+                }
+            }
+        });
+
+    }
+
+    void getUsername(){
+        setInProgress(true);
+        FirebaseUtil.currentUserDetails().get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                setInProgress(false);
+                if(task.isSuccessful()){
+                     userModel = task.getResult().toObject(UserModel.class);
+                    if(userModel!=null){
+                        usernameInput.setText(userModel.getUsername());
+                    }
+                }
+            }
+        });
+    }
+
+    void setInProgress(boolean inProgress){
+        if(inProgress){
+            progressBar.setVisibility(View.VISIBLE);
+            loginEnterBtn.setVisibility(View.GONE);
+        }else{
+            progressBar.setVisibility(View.GONE);
+            loginEnterBtn.setVisibility(View.VISIBLE);
+        }
+    }
+}
